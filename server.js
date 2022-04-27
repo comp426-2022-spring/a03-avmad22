4f272ee503355dae6a9992a2b4b4c46f248857df
avmad22 <avmad22@live.unc.edu>
2022-04-27 02:28:41 -0400

    flips

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const app = express()
 
-// command line arg instead of 5000
-var port = 5000
 
-const server = app.listen(port, () => {
-    console.log('App is running on port %PORT%'.replace('%PORT%', port))
-}) 
+// Start an app server
+const server = app.listen(HTTP_PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+});
 
-app.get('/app', (req, res) => {
-    res.status(200).end('OK')
-    res.type('text/plain')
-})
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
+
+
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+        res.statusCode = 200;
+    // Respond with status message "OK"
+        res.statusMessage = 'OK';
+        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' +res.statusMessage)
+    });
+    
 
 function coinFlip() {
  
@@ -19,10 +29,18 @@ function coinFlip() {
     
     if(num==0) {return "heads"};
     return "tails";
-  }
+}
 
-app.get('/app/flip/', (req, res) => {
-    res.status(200).json({'flip': coinFlip()})
+function coinFlips(flips) {
+    let arr= [];
+    for(let i=0; i<flips; i++) {
+      arr[i] = coinFlip();
+    }
+    return arr;
+}
+
+app.get('/app/flips/:number', (req, res) => {
+    res.status(200).json({'flip': coinFlips(req.params.number)})
 })
 
 app.use(function(req, res) {
