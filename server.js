09cc48e725bdfa3d80b0450366683cfe9f4b5138
avmad22 <avmad22@live.unc.edu>
2022-04-27 02:14:46 -0400

    adding more stuff

--- server.js
+++ server.js
@@ -8,3 +8,16 @@ const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%', port))
 }) 
 
+app.get('/app', (req, res) => {
+    res.status(200).end('OK')
+    res.type('text/plain')
+})
+
+app.get('/app/echo/:number', (req, res) => {
+    res.status(200).json({'message': req.params.number})
+})
+
+app.use(function(req, res) {
+    res.status(404).end("Endpoint does not exist")
+    res.type("text/plain")
+})
