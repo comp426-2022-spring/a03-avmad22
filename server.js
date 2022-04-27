b44c77386adbc41f1570e15de1ff6c5518058a08
avmad22 <avmad22@live.unc.edu>
2022-04-27 01:56:43 -0400

    getting started

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+// command line arg instead of 5000
+var port = 5000
+
+const server = app.listen(port, () => {
+    console.log('App is running on port %PORT%'.replace('%PORT%', port))
+}) 
